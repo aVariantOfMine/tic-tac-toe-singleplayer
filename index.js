aeb80d006628c6d9da31537cb977defbb2a19329
akn714 <adarshkumar20012704@gmail.com>
2024-09-04 16:32:41 +0530

    added partial code of tic-tac-toe

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import express from "express";
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
 
 const app = express();
 const port = 9000;
 app.use("/", (req, res) => {
-  res.json({ message: "Hello From Express App" });
+  res.json({ message: "Hi" });
 });
+
+io.on('connection',(socket)=>{
+  console.log('a player connected');
+  socket.on('disconnect', ()=>{
+      console.log('player disconnected');
+  })
+
+  socket.on('player name', (name)=>{
+      io.emit('pn', name);
+  })
+  socket.on('player selected', obj =>{
+      // io.emit('ps',obj);
+      socket.broadcast.emit('ps',obj);
+  })
+  socket.on('game reset',()=>{
+      socket.broadcast.emit('gr');
+  })
+})
 
 app.listen(9000, () => {
   console.log(`Starting Server on Port ${port}`);
