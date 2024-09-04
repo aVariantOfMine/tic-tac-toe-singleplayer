ae1f4a5fc63dbc094ad3fb42f6649fa64d6f38d2
akn714 <adarshkumar20012704@gmail.com>
2024-09-04 17:54:01 +0530

    fixed errors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
 app.use('/static', express.static(path.join(__dirname, '/static')));
 
 app.get('/', (req, res)=>{
@@ -8,6 +10,6 @@ app.get('/', (req, res)=>{
     // res.send('<h1>hello</h1>');
 })
 
-server.listen(8000 , ()=>{
+app.listen(8000 , ()=>{
     console.log('game running on 8000');
 })
